56fa735e5a73e8bb314465d81b69927d13506171
Sugyo-In <sgin0821@gmail.com>
2020-05-09 06:46:40 +0900

    [Update] geometry.dart 추가
    기하 연산 모듈 추가.

--- lib/flutter_naver_map_plugin.dart
+++ lib/flutter_naver_map_plugin.dart
@@ -1,8 +1,11 @@
 library flutter_naver_map_plugin;
 
+import 'dart:math' as math;
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
+part 'src/geometry.dart';
 part 'src/naver_map.dart';
